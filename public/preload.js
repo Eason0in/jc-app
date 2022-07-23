85cb44d9b9d1f20163ff7170e711928f614eaf39
Eason <leoling127@gmail.com>
2022-07-23 23:07:01 +0800

    整理程式碼

--- public/preload.js
+++ public/preload.js
@@ -8,13 +8,10 @@ contextBridge.exposeInMainWorld('electronAPI', {
   sendBeamMaterialFile: (callback) => ipcRenderer.on('beam-material-file', callback),
   sendBeamConstructionFile: (callback) => ipcRenderer.on('beam-construction-file', callback),
 
-  insertFile: (filePath) => {
-    ipcRenderer.invoke('insert-file', filePath)
-  },
-
   columnReadFile: (filePath) => {
     ipcRenderer.invoke('column-read-file', filePath)
   },
   sendColumnMaterialFile: (callback) => ipcRenderer.on('column-material-file', callback),
+
   sendColumnTidyFile: (callback) => ipcRenderer.on('column-tidy-file', callback),
 })
